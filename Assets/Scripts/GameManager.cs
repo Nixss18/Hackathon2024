abcdcdf40ad77211262580f1848b1ec35a9a6364
raivis.tarakanovs <raivis.tarakanovs@va.lv>
2024-06-15 20:14:40 +0300

    death screen, some adjustments to logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,25 +7,40 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    [SerializeField] GameObject blackOut;
 
     //starts to spawn only when user jumps on the arena
     public bool gameStarted { get; set; }
     public int drinkCount { get; set; }
     public int paddleCount { get; set; }
 
+
     void Awake()
     {
         gameStarted = false;
         drinkCount = 0;
         paddleCount = 0;
+
         if (instance == null) instance = this;
         else Destroy(gameObject);
     }
 
+    void Update()
+    {
+
+        if (drinkCount == 5)
+        {
+            BlackOut();
+        }
+    }
+
     public void BlackOut()
     {
+        blackOut.SetActive(true);
+        GetComponent<AudioSource>().Play();
         StartCoroutine(RestartRoutine());
     }
+
 
     IEnumerator RestartRoutine()
     {
@@ -38,6 +53,6 @@ public class GameManager : MonoBehaviour
     {
         //set a black screen and then restart the scene
         //SceneManager.GetActiveScene().name
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("MainGameScene");
     }
 }
