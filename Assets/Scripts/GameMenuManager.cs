fdaf8fe8e890e8272b9a93df0c51e75b127038f9
Niks <niksgrigors1@gmail.com>
2024-06-15 15:57:42 +0300

    start menu

---
+++ Assets/Scripts/GameMenuManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuManager : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        Debug.Log("Game is starting");
+        SceneManager.LoadScene("MainLevel");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+    public void EndGame()
+    {
+        Debug.Log("Game is ending");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
