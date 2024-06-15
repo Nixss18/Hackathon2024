567b28d8221f052cc94670fc60a12ff96230ed14
abols360 <aleksis.abols@va.lv>
2024-06-15 09:54:12 +0300

    Add skybox

---
+++ Assets/Scripts/SkyBoxManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxManager : MonoBehaviour
+{
+    public float skySpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.deltaTime * skySpeed);
+    }
+}
