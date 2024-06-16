f2d6ec910bb352ca731eb4b93cf7f58eb801404a
abols360 <aleksis.abols@va.lv>
2024-06-16 07:16:21 +0300

    Update movement logic

--- Assets/MoveEnv.cs
+++ Assets/MoveEnv.cs
@@ -9,13 +9,67 @@ public class MoveEnv : MonoBehaviour
 
     // Update is called once per frame
     //public Vector3 direction = Vector3.forward; // Direction of movement
-   public GameObject CoastObjects;
-    private float movementSpeed = 0.03f; // Speed of movement
+    public GameObject Ship;
+    private float maxDistance = 27f;
+    private float movementSpeed = 0.035f; // Speed of movement
+    private Vector3 startPosition;
+
+    bool goesBackwards = true;
+
+
+    void Start()
+    {
+        // Store the initial position of the object
+        startPosition = Ship.transform.position;
+        //movementSpeed = defaultSpeed;
+        
+    }
+
 
     void Update()
     {
         // Move the GameObject slowly in the specified direction
         //this.gameObject.transform.Translate(direction * speed * Time.deltaTime);
-        CoastObjects.transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+       
+        if (goesBackwards == true)
+        {
+            if (Vector3.Distance(startPosition, Ship.transform.position) <= maxDistance)
+            {
+                Ship.transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+                Debug.Log(Vector3.Distance(startPosition, Ship.transform.position) + "t"); 
+                // Reset the object's position to the starting position
+            // Ship.transform.position = startPosition;
+            }
+            else
+            {
+                goesBackwards = false;
+            }
+        }
+        else
+        {
+            if (Vector3.Distance(startPosition, Ship.transform.position) >= 0.5)
+            {
+                Ship.transform.position += Vector3.right * -movementSpeed * Time.deltaTime;
+                Debug.Log(Vector3.Distance(startPosition, Ship.transform.position)  + "F"); 
+                // Reset the object's position to the starting position
+            // Ship.transform.position = startPosition;
+            }
+            else
+            {
+                goesBackwards = true;
+            }
+        }
+
+        // if (Vector3.Distance(startPosition, Ship.transform.position) <= maxDistance)
+        // {
+        //     Ship.transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+        //     Debug.Log(Vector3.Distance(startPosition, Ship.transform.position)); 
+        //     // Reset the object's position to the starting position
+        //    // Ship.transform.position = startPosition;
+        // }
+        
+
+
+        
     }
 }
